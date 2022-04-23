122a415fcfe139dddf51adb03ab9d72395a5c1dd
Tom <t.p.kellett@gmail.com>
2022-04-23 14:37:16 +0100

    adding session support and building out components

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,9 +11,7 @@ export default class App extends Component {
 
   render() {
     return ( 
-      <div>
         <HomePage />
-      </div>
     );
   }
 }
--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,6 @@ from .views import index
 urlpatterns = [
     path('', index),
     path('join', index),
-    path('create-room', index)
+    path('create-room', index),
+    path('room/<str:roomCode>', index)
 ]
